2910ee6cdc249f3d18eb8d49dbf29fc84f9d850f
Pavel Naidenov <pavel.naidenov@workato.com>
2025-10-01 19:12:35 +0400

    handle empty datetime/datetime64 values

--- lib/clickhouse/connection/query/result_set.rb
+++ lib/clickhouse/connection/query/result_set.rb
@@ -66,7 +66,7 @@ module Clickhouse
             when /DateTime\(/
               parse_date_time_value(value, type.scan(/DateTime\('(.*)'\)/)&.first&.first)
             when /DateTime64\(/
-              parse_date_time64_value(value, type.scan(/DateTime64\(\d+,\s*'(.+)'\)/)&.first&.first)
+              parse_date_time_value(value, type.scan(/DateTime64\(\d+,\s*'(.+)'\)/)&.first&.first)
             when /Array\(/
               parse_array_value value
             else
@@ -102,12 +102,9 @@ module Clickhouse
         end
 
         def parse_date_time_value(value, tz=nil)
-          return nil if value == '0000-00-00 00:00:00'
-          tz ? Time.parse(value + " " + tz) : Time.parse(value)
-        end
-
-        def parse_date_time64_value(value, tz=nil)
           return nil if value.start_with? '1970-01-01 00:00:00'
+          return nil if tz && value.start_with?('1970-01-01') # datetime with tz starts from non-zero hour
+
           tz ? Time.parse(value + " " + tz) : Time.parse(value)
         end
 
